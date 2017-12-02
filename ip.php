b7034a3cb8bb616830006f2001ec9080b49645d4
PrintNow <chenwenzhou@aliyun.com>
2017-12-02 13:28:33 +0800

    更改查询IP地址API
    API 由 ip.NowTool.cn 提供，Demo：https://ip.nowtool.cn/api.php?do=ipbach&ip=nowtool.cn

--- ip.php
+++ ip.php
@@ -1,8 +1,10 @@
 <?php
+if(!isset($_GET['ip']))die('bad Request');
+
 function curl($url,$data=null){
     $curl=curl_init();
-    curl_setopt($curl,CURLOPT_URL,$url);
-    curl_setopt($curl,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($curl,CURLOPT_URL, $url);
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
     curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1);
     curl_setopt($curl, CURLOPT_HTTPHEADER, Array('User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36','Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8','Referer: http://www.ipip.net/'));
@@ -14,23 +16,13 @@ function curl($url,$data=null){
     curl_close($curl);
     return $result;
 }
-function getSubstr($str, $leftStr, $rightStr)
-{
-    $left = strpos($str, $leftStr);
-    $right = strpos($str, $rightStr,$left);
-    if($left < 0 or $right < $left) return '';
-    return substr($str, $left + strlen($leftStr), $right-$left-strlen($leftStr));
-}
 
-$data = curl('http://www.ipip.net/ip.html','ip='.$_GET['ip']);
-$ip = getSubstr($data,'<span id="myself">','</span>');
+$data=json_decode(curl_get('https://ip.nowtool.cn/api.php?do=ipbach&ip='.$_GET['ip']));
 
-echo $ip;
-
-function test(){
-return 'dd';
-}
-
-function test_one(){
-return 'dd';
+if($data['code']=='200'){
+  echo $data['data']['location'];
+}elseif($data['data']['code']=='404'){
+  echo 'IP 地址为空或IP地址不合法';
+}else{
+  echo '服务器与 ip.NowTool.cn 通信失败，请稍后再试！';
 }
