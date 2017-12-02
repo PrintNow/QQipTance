83f27d6c12feeda70b96591b6bb03f0e3bd2f908
PrintNow <chenwenzhou@aliyun.com>
2017-12-02 13:31:30 +0800

    更改查询IP地址API
    API Demo: https://ip.nowtool.cn/api.php?do=ipbach&ip=8.8.8.8
    
    API Demo2: https://ip.nowtool.cn/api.php?do=ipbach&ip=nowtime.cc

--- ip.php
+++ ip.php
@@ -1,12 +1,14 @@
 <?php
 if(!isset($_GET['ip']))die('bad Request');
 
+//$_GET['ip']='114.114.114.114';
+
 function curl($url,$data=null){
     $curl=curl_init();
     curl_setopt($curl,CURLOPT_URL, $url);
-    curl_setopt($curl,CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl,CURLOPT_RETURNTRANSFER, false);
     curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 1);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
     curl_setopt($curl, CURLOPT_HTTPHEADER, Array('User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.110 Safari/537.36','Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8','Referer: http://www.ipip.net/'));
     if($data){
         curl_setopt($curl,CURLOPT_POSTFIELDS,$data);
@@ -17,7 +19,7 @@ function curl($url,$data=null){
     return $result;
 }
 
-$data=json_decode(curl_get('https://ip.nowtool.cn/api.php?do=ipbach&ip='.$_GET['ip']));
+$data=json_decode(curl('https://ip.nowtool.cn/api.php?do=ipbach&ip='.$_GET['ip']));
 
 if($data['code']=='200'){
   echo $data['data']['location'];
